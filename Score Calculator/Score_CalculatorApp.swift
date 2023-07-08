836074d3c00414ab26011d6f9f05ac171a50b862
Abdulloh <abdullohbahromjonv@gmail.com>
2023-07-08 22:47:27 +0500

    Initial Commit

---
+++ Score Calculator/Score_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Score_CalculatorApp.swift
+//  Score Calculator
+//
+//  Created by Abdulloh on 08/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Score_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
